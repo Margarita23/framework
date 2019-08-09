b62769bf67268d854d805fbca42d556aa28542a8
margarita <margarita.andriukhina@gmail.com>
2019-08-09 16:31:44 +0300

    .

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,6 @@ import { Application } from "./models/application";
 // const view = new SomeView();
 // const controller = new SomeController(view);
 let app = Application.instance;
+
+export let application = Application.instance;
 // app.run(view);
