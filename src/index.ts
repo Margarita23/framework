6304bca5583299a02c08f1945f9c847afcfae7e7
margarita <margarita.andriukhina@gmail.com>
2019-08-15 09:32:14 +0300

    +

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Application } from "./models/application";
 // const view = new SomeView();
 // const controller = new SomeController(view);
+let canvas = document.createElement("canvas").setAttribute("id", "canvas");
 export const application = Application.instance;
 // app.run(view);
